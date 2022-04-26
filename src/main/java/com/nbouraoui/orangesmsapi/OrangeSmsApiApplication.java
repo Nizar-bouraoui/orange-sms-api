dd4637722ea05cc80fb39e2fe8c57366bc53e4af
Nizar-bouraoui <nizar.bouraoui7@gmail.com>
2022-04-26 09:48:38 +0000

    init project.

---
+++ src/main/java/com/nbouraoui/orangesmsapi/OrangeSmsApiApplication.java
@@ -0,0 +1,13 @@
+package com.nbouraoui.orangesmsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeSmsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangeSmsApiApplication.class, args);
+	}
+
+}
