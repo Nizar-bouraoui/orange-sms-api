f32fd3ac532f87c723e6ba45920b96a11c9f89fe
Nizar-bouraoui <nizar.bouraoui7@gmail.com>
2022-04-26 11:15:28 +0000

    implement AccessTokenManager

--- src/main/java/com/nbouraoui/orangesmsapi/OrangeSmsApiApplication.java
+++ src/main/java/com/nbouraoui/orangesmsapi/OrangeSmsApiApplication.java
@@ -3,11 +3,19 @@ package com.nbouraoui.orangesmsapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.nbouraoui.orangesmsapi.services.AccessTokenManager;
+
 @SpringBootApplication
 public class OrangeSmsApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrangeSmsApiApplication.class, args);
+		AccessTokenManager accessTokenManager = new AccessTokenManager();
+//		String test = accessTokenManager.generateAccessToken("Basic V2dyNWtQaHFhUjhHQUFUOXlnWkhmd3RuZXVBcUFvRDE6UGNVYzFzZ21pemh4NUJoOQ==");
+		String test = accessTokenManager.generateAccessToken("Wgr5kPhqaR8GAAT9ygZHfwtneuAqAoD1","PcUc1sgmizhx5Bh9");
+
+		System.out.println(test);
+
 	}
 
 }
